3e51233f5bf8ea49d5111ec0b068ddedc8db6985
தேசிகன் <136635078+DhesiTheKing@users.noreply.github.com>
2024-01-08 10:21:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,67 @@
-function disEl(){
-    document.getElementById("ans").style.display="none";
-}
+
+let Matchcount=0;
+let winCount=0;
+let looseCount=0;
+let tieCount=0;
 function userData(e){
+    Matchcount++;
     let computer=Math.floor(Math.random() * (4-1) + 1);
-    if(computer==1 && e==2 || computer==2 && e==3 || computer==3 && e==1){
-        document.getElementById("ans").style.display="flex";
-        document.getElementById("answer").innerHTML="YOU WON!!";
-        setTimeout(disEl,10000);
-    }
-    else if(computer==e){
-        document.getElementById("ans").style.display="flex";
-        document.getElementById("answer").innerHTML="MATCH DIE!!";
-        setTimeout(disEl,10000);
+    if(e==1){
+        document.getElementById("u1").style.display="flex";
     }
     else{
+        document.getElementById("u1").style.display="none";
+    }
+    if(e==2){
+        document.getElementById("u2").style.display="flex";
+    }
+    else{
+        document.getElementById("u2").style.display="none";
+    }
+    if(e==3){
+        document.getElementById("u3").style.display="flex";
+    }
+    else{
+        document.getElementById("u3").style.display="none";
+    }
+    if(computer==1){
+        document.getElementById("c1").style.display="flex";
+    }
+    else{
+        document.getElementById("c1").style.display="none";
+    }
+    if(computer==2){
+        document.getElementById("c2").style.display="flex";
+    }
+    else{
+        document.getElementById("c2").style.display="none";
+    }
+    if(computer==3){
+        document.getElementById("c3").style.display="flex";
+    }
+    else{
+        document.getElementById("c3").style.display="none";
+    }
+    if(computer==1 && e==2 || computer==2 && e==3 || computer==3 && e==1){
+        winCount++;
+        document.getElementById("ans").style.display="flex";
+        document.getElementById("answer").innerHTML="YOU WON!!";
+    }
+    else if(computer==e){
+        tieCount++;
+        document.getElementById("ans").style.display="flex";
+        document.getElementById("answer").innerHTML="MATCH TIE!!";
+    }
+    else{
+        looseCount++;
         document.getElementById("ans").style.display="flex";
         document.getElementById("answer").innerHTML="YOU LOOSE!!";
-        setTimeout(disEl,10000);
     }
+    document.getElementById("matchCount").innerHTML=Matchcount;
+    document.getElementById("winCount").innerHTML=winCount;
+    document.getElementById("looseCount").innerHTML=looseCount;
+    document.getElementById("tieCount").innerHTML=tieCount;
+}
+function reset(){
+    location.reload();
 }
